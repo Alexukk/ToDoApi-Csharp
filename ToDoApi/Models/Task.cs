25a4c822e9f895c7fce913ccffbbbd25d427e8da
Alex <alexukrok@gmail.com>
2025-11-20 21:37:52 +0200

    Entities for DB created

---
+++ ToDoApi/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApi.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
